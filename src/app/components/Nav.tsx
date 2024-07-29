2cf06406cd9e0fb3ba0037d0cddd8858f25c8c52
Naresh123se <naresh132na@gmail.com>
2024-07-29 23:29:11 +0545

    added gsap to

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -10,54 +10,54 @@ import gsap from "gsap";
 
 
 const Nav = () => {
-const ti = gsap.timeline();
-  useGSAP(() =>{
+  const ti = gsap.timeline();
+  useGSAP(() => {
     ti.from(".l, .nav, .logo1, .logo2", {
-      y:-30,
-      opacity:0,
+      y: -30,
+      opacity: 0,
       duration: 1,
       delay: 1,
-      stagger:0.2,
+      stagger: 0.2,
     })
 
     gsap.from(".logo", {
-      x:-500,
+      x: -500,
       duration: 2,
       delay: 1
     })
-    
+
   })
   return (
     <Wrap>
-    <div className="h-20">
-      {/* Mobile */}
-      <div className="h-full flex items-center justify-between md:hidden ">
-        <div className="text-2xl ">
-          <Link href="/">Lama</Link>
+      <div className="h-20">
+        {/* Mobile */}
+        <div className="h-full flex items-center justify-between md:hidden ">
+          <div className="text-2xl ">
+            <Link href="/">Lama</Link>
+          </div>
+          <Menu />
         </div>
-        <Menu />
-      </div>
 
-      {/* bigger */}
-      <div className="hidden md:flex items-center justify-between gap-8 h-full">
-        <div className="text-lg nav">
-          <Link href="/">LOGO</Link>
-        </div>
-        <div className="hidden xl:flex gap-4">
-          
-          <Link href="/" className="l">Home</Link>
-          <Link href="/" className="l">Shop</Link>
-          <Link href="/" className="l">About</Link>
-          <Link href="/" className="l">Contact</Link>
-        </div>
-        <div className=" w-2/3 items-center justify-between gap-8 flex logo1">
-          <SearchBar />
-          <div className="logo2">
-          <NavIcons />
+        {/* bigger */}
+        <div className="hidden md:flex items-center justify-between gap-8 h-full">
+          <div className="text-lg nav">
+            <Link href="/">LOGO</Link>
+          </div>
+          <div className="hidden xl:flex gap-4">
+
+            <Link href="/" className="l">Home</Link>
+            <Link href="/" className="l">Shop</Link>
+            <Link href="/" className="l">About</Link>
+            <Link href="/" className="l">Contact</Link>
+          </div>
+          <div className=" w-2/3 items-center justify-between gap-8 flex logo1">
+            <SearchBar />
+            <div className="logo2">
+              <NavIcons />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Wrap>
   );
 };
